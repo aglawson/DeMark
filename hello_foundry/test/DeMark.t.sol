56f87f4c6c573a49ce1eca7c7ff7c93591af12c8
Adrian Lawson <adrianglawson@gmail.com>
2023-08-31 11:44:00 -0700

    test correct deployment

--- hello_foundry/test/DeMark.t.sol
+++ hello_foundry/test/DeMark.t.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console2} from "../lib/forge-std/src/Test.sol";
 import {DeMark} from "../src/DeMark.sol";
 
 contract CounterTest is Test {
     DeMark public demark;
 
     function setUp() public {
-        demark = new DeMark();
+        demark = new DeMark(10);
+    }
+
+    function test_platformFeeIs10() public {
+        assertEq(10, demark.platformFee());
     }
 
 }
