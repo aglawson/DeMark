c3c9fc6a9e67008eddec4f64185a1f4f5303bd7d
Adrian Lawson <adrianglawson@gmail.com>
2023-08-31 10:28:27 -0700

    add ratings logic

--- hello_foundry/src/DeMark.sol
+++ hello_foundry/src/DeMark.sol
@@ -5,9 +5,20 @@ import {Ownable} from "./Ownable.sol";
 import {IDeMark} from "./IDeMark.sol";
 contract DeMark is Ownable, IDeMark {
     uint256 public platformFee;
+    uint256 public accumulatedFees;
 
     Job[] public jobs;
-    constructor(uint256 _platformFee) {
+
+    /**
+        stores ratings on a 1-5 scale for proposers and laborers
+        ratings[userAddress]['proposer'] => the array of ratings of userAddress as a proposer
+        ratings[userAddress]['completor'] => the array of ratings of userAddress as a completor
+     */
+    mapping(address => mapping(string => uint8[])) public ratings;
+
+    constructor(uint256 _platformFee) Ownable(_msgSender()) {
+        require(_platformFee > 0, "Platform fee must be > 0");
+        
         platformFee = _platformFee;
     }
 
@@ -18,25 +29,57 @@ contract DeMark is Ownable, IDeMark {
         _;
     }
 
+    modifier onlyCompletor(uint256 jobId) {
+        if(_msgSender() != jobs[jobId].completedBy) {
+            revert NotCompletor();
+        }
+        _;
+    }
+
     function setPlatformFee(uint256 _platformFee) external payable onlyOwner {
         platformFee = _platformFee;
     }
 
     function proposeJob(string memory _jobDescription) external payable override {
-        jobs.push(Job(_msgSender(), msg.value, _jobDescription, address(0)));
+        jobs.push(Job(_msgSender(), msg.value, _jobDescription, address(0), block.timestamp, 0, 0, 0));
 
         emit JobCreated(_msgSender(), msg.value, _jobDescription);
     }
 
-    function markComplete(uint256 jobId, address _completedBy) external payable override {
+    function markComplete(uint256 jobId, address _completedBy, uint8 rating) external payable override {
         if(jobs[jobId].completedBy == address(0)) {
             revert AlreadyCompleted();
         }
         jobs[jobId].completedBy = _completedBy;
 
-        uint256 fee = jobs[jobId].payout / platformFee;
+        if(rating < 1 || rating > 5) {
+            revert MustBeBetweenOneAndFiveInclusive();
+        }
+        jobs[jobId].completorRating = rating;
+
+        jobs[jobId].completedAt = block.timestamp;
+
+        uint256 fee = (jobs[jobId].payout / 100) * platformFee;
         uint256 finalPayout = jobs[jobId].payout - fee;
 
+        accumulatedFees += fee;
+
         (bool success,) = _completedBy.call{value: finalPayout}("");
+        require(success, "payout failed");
+
+        emit JobCompleted(_completedBy, jobId);
     }
+
+    function rateProposer(uint256 jobId, uint8 rating) external payable override onlyCompletor(jobId) {
+        if(jobs[jobId].proposerRating != 0) {
+            revert AlreadyRated();
+        }
+        
+        if(rating < 1 || rating > 5) {
+            revert MustBeBetweenOneAndFiveInclusive();
+        }
+        ratings[jobs[jobId].proposer]['proposer'].push(rating);
+    }
+
+
 }
--- hello_foundry/src/IDeMark.sol
+++ hello_foundry/src/IDeMark.sol
@@ -5,6 +5,9 @@ interface IDeMark {
 
     error AlreadyCompleted();
     error NotProposer();
+    error NotCompletor();
+    error MustBeBetweenOneAndFiveInclusive();
+    error AlreadyRated();
 
     event JobCreated(address proposer, uint256 payout, string jobDescription);
     event JobCompleted(address completedBy, uint256 jobId);
@@ -13,6 +16,10 @@ interface IDeMark {
         uint256 payout;
         string jobDescription;
         address completedBy;
+        uint256 createdAt;
+        uint256 completedAt;
+        uint8 proposerRating;
+        uint8 completorRating;
     }
 
     /**
@@ -23,5 +30,10 @@ interface IDeMark {
     /**
         @dev emits a 'JobCompleted' event
     */
-    function markComplete(uint256 jobId, address _completedBy) external payable;
+    function markComplete(uint256 jobId, address _completedBy, uint8 rating) external payable;
+
+    /**
+        @dev allows job completer to rate their experience with the job proposer
+     */
+    function rateProposer(uint256 jobId, uint8 rating) external payable;
 }
